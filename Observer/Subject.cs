ff97a10ad6e9edf9d68caed0e9c98e9b8ae26db2
wangyoucao577@gmail.com <wangyoucao577@gmail.com>
2015-12-09 17:59:56 +0800

    添加几个漏掉上传的文件。 添加BaseSubject以更好的抽象主题。

--- Observer/Subject.cs
+++ Observer/Subject.cs
@@ -6,28 +6,10 @@ using System.Threading.Tasks;
 
 namespace Observer
 {
-    class Subject
+    class Subject : BaseSubject
     {
-        protected List<IObserver> m_objectList = new List<IObserver>();
-
         public virtual int TimeAsSecond { get; }
-
-        public void Attach(IObserver obj)
-        {
-            m_objectList.Add(obj);
-        }
-
-        public void Detach(IObserver obj)
-        {
-            m_objectList.Remove(obj);
-        }
-
-        public void DetachAll()
-        {
-            m_objectList.Clear();
-        }
-
-        public void Notify()
+        public override void Notify()
         {
             foreach (IObserver item in m_objectList)
             {
---
+++ Observer/TextboxObserver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Observer
+{
+    class TextboxObserver : TextBox, IObserver
+    {
+        public void Update(Subject sub)
+        {
+            this.SetText(sub.TimeAsSecond.ToString());
+        }
+
+        // This delegate enables asynchronous calls for setting
+        // the text property on a TextBox control.
+        delegate void SetTextCallback(string text);
+
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.Text = text;
+            }
+
+        }
+    }
+}
