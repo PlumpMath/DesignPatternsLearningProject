2a09587ec2b406996cd538c30c82ff338bf53c9e
wangyoucao577@gmail.com <wangyoucao577@gmail.com>
2015-12-09 22:12:19 +0800

    漏掉上传的文件。

---
+++ Observer/ButtonObserver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Observer
+{
+    class ButtonObserver : Button, IObserver
+    {
+        public void Update (object sub)
+        {
+            ObserverState newState = (ObserverState)sub;
+
+            if (newState == ObserverState.Observing)
+            {
+                Enabled = true;
+            }
+            else
+            {
+                Enabled = false;
+            }
+        }
+    }
+}
