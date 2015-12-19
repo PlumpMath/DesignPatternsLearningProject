6c344647ccee137980aae750d747caf5ff02bc03
wangyoucao577@gmail.com <wangyoucao577@gmail.com>
2015-12-20 00:09:25 +0800

    初步的main功能实现。待调试。

--- observer_c/main.cpp
+++ observer_c/main.cpp
@@ -8,13 +8,66 @@ Description	: main
 #include "common_include.h"
 #include "subject.h"
 
+
+struct my_info_subject_t
+{
+	int my_id;
+	char my_description[128];
+};
+
+static int observer_A_update(void* param)
+{
+	assert(NULL != param);
+	my_info_subject_t * my_info = (my_info_subject_t*)param;
+	printf("%s:my new info----------->id:%d description:%s.\n", \
+		__FUNCTION__, my_info->my_id, my_info->my_description);
+
+	return NO_ERROR;
+}
+
+static int observer_B_update(void* param)
+{
+	assert(NULL != param);
+	my_info_subject_t * my_info = (my_info_subject_t*)param;
+	printf("%s:my new info----------->id:%d description:%s.\n", \
+		__FUNCTION__, my_info->my_id, my_info->my_description);
+
+	return NO_ERROR;
+}
+
 int main()
 {
-	printf("That's a test.\n");
+	printf("Observer test.\n");
 
-	while (1)
-	{
-		Sleep(1);
-	}
+	subject_model_t subject_mode;
+	initialize_subject_model_t(&subject_mode);
+	printf("subject model has been created.\n");
+
+	subject_mode.attach_observer(&subject_mode, observer_A_update);
+	printf("Observer A has been attached.\n");
+
+	my_info_subject_t new_info;
+	memset(&new_info, 0, sizeof(new_info));
+	printf("Please Enter the new id:");
+	scanf_s("%d", &new_info.my_id);
+	printf("Please Enter the new description:");
+	scanf("%s", new_info.my_description);
+	subject_mode.notify(&subject_mode, &new_info);
+	printf("\n\n");
+
+	subject_mode.attach_observer(&subject_mode, observer_B_update);
+	printf("Obsever B has been attached.\n");
+
+	memset(&new_info, 0, sizeof(new_info));
+	printf("Please Enter the new id:");
+	scanf_s("%d", &new_info.my_id);
+	printf("Please Enter the new description:");
+	scanf("%s", new_info.my_description);
+	subject_mode.notify(&subject_mode, &new_info);
+
+	printf("\n\nPlease Enter any key to continue...\n");
+	char any_key = '0';
+	scanf("%c", &any_key);
+
 	return 0;
 }
