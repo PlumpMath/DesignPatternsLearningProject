e98a44b28ee2e79f3dc6d290346dc0af02e05ec5
wangyoucao577@gmail.com <wangyoucao577@gmail.com>
2015-12-19 23:35:41 +0800

    subject功能代码。

--- observer_c/subject.cpp
+++ observer_c/subject.cpp
@@ -10,12 +10,61 @@ Description	: implementation of subject
 #include "list.h"
 #include "subject.h"
 
+static int attach_observer(struct subject_model_t* subject_model, subject_update_callback update)
+{
+	assert(NULL != subject_model);
+	assert(NULL != update);
+
+	struct node_t* new_node = (struct node_t*)malloc(sizeof(struct node_t));
+	assert(NULL != new_node);
+
+	new_node->data = update;
+	new_node->next = NULL;
+
+	return subject_model->callback_list->push(subject_model->callback_list, new_node);
+}
+
+static int detach_observer(struct subject_model_t* subject_model, subject_update_callback update)
+{
+	//TODO: 
+	return COMMON_ERROR_CODE_UNAVAILABLE;
+}
+static int detach_all(struct subject_model_t* subject_model)
+{
+	assert(NULL != subject_model);
+
+	return subject_model->callback_list->clear(subject_model->callback_list);
+}
+static int notify(struct subject_model_t* subject_model, void* subject)
+{
+	assert(NULL != subject_model);
+	assert(NULL != subject);
+
+	int count = subject_model->callback_list->count(subject_model->callback_list);
+	for (int i = 0; i < count; ++i)
+	{
+		struct node_t* curr_node = NULL; 
+		int retval = subject_model->callback_list->peek(subject_model->callback_list, i, curr_node);
+		assert(NO_ERROR == retval);
+
+		int callback_retval = ((subject_update_callback)(curr_node->data))(subject);
+	}
+
+	return NO_ERROR;
+}
+
+
 int initialize_subject_model_t(struct subject_model_t* subject_model)
 {
 	assert(NULL != subject_model);
 
 	initialize_list_t(subject_model->callback_list);
 
+	//initialize interfaces
+	subject_model->attach_observer = attach_observer;
+	subject_model->detach_observer = detach_observer;
+	subject_model->detach_all = detach_all;
+	subject_model->notify = notify;
 
 	return NO_ERROR;
 }
--- observer_c/subject.h
+++ observer_c/subject.h
@@ -10,23 +10,26 @@ Description	: define my structs for the observer design
 
 typedef int(*subject_update_callback)(void* param);
 
-struct subject_t
+struct subject_model_t
+{
+	struct list_t* callback_list;
+
+	int (*attach_observer)(struct subject_model_t* subject_model, subject_update_callback update);
+	int (*detach_observer)(struct subject_model_t* subject_model, subject_update_callback update);
+	int (*detach_all)(struct subject_model_t* subject_model);
+	int (*notify)(struct subject_model_t* subject_model, void* subject);
+};
+
+int initialize_subject_model_t(struct subject_model_t* subject_model);
+
+
+
+struct my_subject_t
 {
 	int my_id;
 	char my_description[128];
 };
 
-struct subject_model_t
-{
-	struct list_t* callback_list;
-
-	int (*attach_observer)(subject_update_callback update);
-	int (*detach_observer)(subject_update_callback update);
-	int (*detach_all)();
-	int (*notify)(void* subject);
-};
-
-int initialize_subject_model_t(struct subject_model_t* subject_model);
 
 #endif // __SUBJECT_H__
 
