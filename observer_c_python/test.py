a0c5d0cfb263df2d8ef880e0bd3548e9863b634b
wangyoucao577@gmail.com <wangyoucao577@gmail.com>
2015-12-30 18:29:49 +0800

    添加python的test文件，目前仅一个文件，后续慢慢扩展为项目。

---
+++ observer_c_python/test.py
@@ -0,0 +1,14 @@
+#!/bin/env python
+# coding=UTF-8
+
+import os
+from ctypes import *
+
+common_lib_c_path = r"../Debug/common_lib_c.dll";
+if os.path.exists(common_lib_c_path):
+    common_lib_c = cdll.LoadLibrary(common_lib_c_path);
+    print(common_lib_c)
+    print(common_lib_c.log_print)
+    print(common_lib_c.initialize_list_t)
+    i = 5
+    common_lib_c.log_print("test")
