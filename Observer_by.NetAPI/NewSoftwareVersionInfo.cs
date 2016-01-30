f2b1005ecc8b8ffec4ade048b7ef9e0561594208
wangyoucao577@gmail.com <wangyoucao577@gmail.com>
2016-01-30 23:42:19 +0800

    采用.Net提供的Observer模式的支持，动手写个小sample。主题暂定为新版本发布的发布-订阅。

---
+++ Observer_by.NetAPI/NewSoftwareVersionInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Observer_by.NetAPI
+{
+    class NewSoftwareVersionInfo
+    {
+        private string versionNumber;
+        private string description;
+
+        NewSoftwareVersionInfo(string versionNumber, string description)
+        {
+            this.versionNumber = versionNumber;
+            this.description = description;
+        }
+
+        public string VersionNumber
+        {
+            get { return versionNumber; }
+            set { versionNumber = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+    }
+}
