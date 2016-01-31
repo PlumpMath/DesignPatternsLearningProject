102e72e0a252d7ae4ea72c39a62a69fb466733d6
wangyoucao577@gmail.com <wangyoucao577@gmail.com>
2016-01-31 15:05:03 +0800

    .Net提供的接口实现的Observer Sample.

--- Observer_by.NetAPI/Program.cs
+++ Observer_by.NetAPI/Program.cs
@@ -8,6 +8,38 @@ namespace Observer_by.NetAPI
     {
         static void Main(string[] args)
         {
+            NewVersionPublishHandler newVerPublisher = new NewVersionPublishHandler();
+
+            PublishMonitor monitorBoard = new PublishMonitor("Publish Board");
+            monitorBoard.Subscribe(newVerPublisher);
+
+            Console.WriteLine("\nGenernate new version:");
+            newVerPublisher.NewVersionGenernate(
+                ReadStringFromConsole("VersionNumber"), ReadStringFromConsole("Description"));
+
+            PublishMonitor monitorTechSupport = new PublishMonitor("Tech Support");
+            monitorTechSupport.Subscribe(newVerPublisher);
+
+            Console.WriteLine("\nGenernate new version:");
+            newVerPublisher.NewVersionGenernate(
+                ReadStringFromConsole("VersionNumber"), ReadStringFromConsole("Description"));
+
+            monitorBoard.Unsubscribe();
+
+            Console.WriteLine("\nGenernate new version:");
+            newVerPublisher.NewVersionGenernate(
+                ReadStringFromConsole("VersionNumber"), ReadStringFromConsole("Description"));
+
+
+            Console.ReadKey();
+        }
+
+        static string ReadStringFromConsole(string itemName)
+        {
+            Console.Write(itemName + ":");
+            string itemValue = Console.ReadLine();
+
+            return itemValue;
         }
     }
 }
---
+++ Observer_by.NetAPI/PublishMonitor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Observer_by.NetAPI
+{
+    class PublishMonitor : IObserver<NewSoftwareVersionInfo>
+    {
+        private string m_name = null;
+        private IDisposable m_cancellation = null;
+
+
+        public PublishMonitor(string name)
+        {
+            m_name = name;
+        }
+
+        public void Subscribe(IObservable<NewSoftwareVersionInfo> provider)
+        {
+            m_cancellation = provider.Subscribe(this);
+            Console.WriteLine("\n{0} Start Observe.", m_name);
+        }
+
+        public void Unsubscribe()
+        {
+            Console.WriteLine("\n{0} Stop Observe.", m_name);
+            if (m_cancellation != null)
+            {
+                m_cancellation.Dispose();
+            }
+        }
+
+        public void OnCompleted()
+        {
+            Console.WriteLine("<{0}> NewVersionPublish Done.", m_name);
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(NewSoftwareVersionInfo value)
+        {
+            Console.WriteLine("\n<{0}> New Software Version here--->{1}:{2}.",
+                m_name, value.VersionNumber, value.Description);
+        }
+    }
+}
