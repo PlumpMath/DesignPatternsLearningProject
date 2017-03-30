8856d2d9f4a8c3bfab5e0799be7f9f3f2be31e0c
wangyoucao577 <wangyoucao577@gmail.com>
2017-03-30 15:58:32 +0800

    chore: 添加 State 模式的示例项目

---
+++ State/State.cpp
@@ -0,0 +1,14 @@
+// State.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Please press any key to continue..." << endl; 
+    _getch();
+    return 0;
+}
+
